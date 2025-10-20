befff05baafa3120f763404cb91e9525d5a25e87
Iratxe <iratxe92@gmail.com>
2025-10-20 10:55:59 +0200

    Pokemon information section

--- src/components/PokemonInformation/Sections/Information.tsx
+++ src/components/PokemonInformation/Sections/Information.tsx
@@ -5,7 +5,7 @@ import { usePokemonCard } from "../../hooks/usePokemonCard";
 export const Information = () => {
     const { data} = usePokemonCard();
     const {data:textData} = usePokemonText(data?.id || 1)
-    console.log(textData)
+
   return (
     <div className="p-4 mt-4 text-base ">
         <p>{textData?.flavor_text_entries[0].flavor_text.replace(/[\n\f\r]+/g, ' ')} </p>
